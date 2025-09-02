c317f44dd19e9b4969f26f3b5f4fb5c91992c346
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-09-02 22:53:53 +0530

    Update 387. First Unique Character in a String.java

--- String_EASY/387. First Unique Character in a String.java
+++ String_EASY/387. First Unique Character in a String.java
@@ -1,15 +1,11 @@
 class Solution {
     public int firstUniqChar(String s) {
-        int[] freq = new int[26]; 
-        for (char c : s.toCharArray()) {
-            freq[c - 'a']++;
-        }
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (freq[s.charAt(i) - 'a'] == 1) {
+        for(int i=0; i<s.length(); i++) {
+            char first = s.charAt(i);
+            if (s.indexOf(first)==(s.lastIndexOf(first))) {
                 return i;
             }
-        }
+         }
         return -1;
     }
 }
