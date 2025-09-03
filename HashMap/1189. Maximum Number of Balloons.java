e5fe147fc092be680f7eaad7d99f72827e4d0298
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-09-03 21:47:45 +0530

    Create 1189. Maximum Number of Balloons.java

---
+++ HashMap/1189. Maximum Number of Balloons.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        Map<Character, Integer> map_text = new HashMap<>();
+        for(char c: text.toCharArray()) {
+            map_text.put(c, map_text.getOrDefault(c,0)+1);
+        }
+
+        String s = "balloon";
+
+        Map<Character, Integer> map_balloon = new HashMap<>();
+        for(char i: s.toCharArray()) {
+            map_balloon.put(i, map_balloon.getOrDefault(i,0) +1);
+        }
+
+        int min = 100000;
+
+        for (char c : map_balloon.keySet()) {
+            int available = map_text.getOrDefault(c, 0);
+            int required = map_balloon.get(c);
+            min = Math.min(min, available / required);
+        }
+
+        System.out.println(map_text);
+        System.out.println(map_balloon);
+
+        return min;
+    }
+}
