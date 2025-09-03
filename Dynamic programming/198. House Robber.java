4b92cd774ba59719a66c979337d4072b3fe967e2
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-09-03 17:28:17 +0530

    Create 198. House Robber.java

---
+++ Dynamic programming/198. House Robber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        return RobberBU(nums);
+    }
+    public static int RobberBU(int[] arr) {
+		if(arr.length==1) return arr[0];
+		int[] dp = new int[arr.length];
+		dp[0] = arr[0];
+		dp[1] = Math.max(arr[0], arr[1]);
+		for(int i=2;i<arr.length;i++) {
+			int rob = arr[i]+dp[i-2];
+			int dont_rob = dp[i-1];
+			dp[i] = Math.max(rob, dont_rob);
+		}
+		return dp[dp.length-1];
+	}
+}
