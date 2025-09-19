bdc86786aacb13e5c312c2249a51aca9c1421b14
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-09-19 19:36:31 +0530

    Create 1598. Crawler Log Folder.java

---
+++ Stack/1598. Crawler Log Folder.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> stack = new Stack<>();
+        for(String s: logs) {
+            if (s.equals("../")) {
+                if (!stack.isEmpty()) stack.pop();
+            }
+            else if (!s.equals("./")) {
+                stack.push(s);
+            }
+        }
+        return stack.size();
+    }
+}
