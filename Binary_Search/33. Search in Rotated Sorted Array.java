c8570bd02bc6f34462dc573ecc7431e17e5b524a
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-09-28 18:55:54 +0530

    Update 33. Search in Rotated Sorted Array.java

--- Binary_Search/33. Search in Rotated Sorted Array.java
+++ Binary_Search/33. Search in Rotated Sorted Array.java
@@ -1,3 +1,4 @@
+// Linear Search
 class Solution {
     public int search(int[] nums, int target) {
         for (int i=0; i<nums.length; i++) {
@@ -5,6 +6,38 @@ class Solution {
                 return i;
             }
         }
+        return -1;
+    }
+}
+
+// Binary Search
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+
+            // left part sorted
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1; 
+                }
+            }
+            
+            //right part sorted
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1; 
+                } else {
+                    right = mid - 1; 
+                }
+            }
+        }
+
         return -1;
     }
 }
