487c56a0204e222fededefa337dd1517c54dd8bd
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-10-05 14:52:07 +0530

    Create 1732. Find the Highest Altitude.java

---
+++ Array_Questions_Easy/1732. Find the Highest Altitude.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int n = gain.length;
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        int max = 0;
+
+        for(int i=1; i<arr.length; i++) {
+            arr[i] = arr[i-1] + gain[i-1];
+            max = Math.max(arr[i], max);
+        }
+        return max;
+    }
+}
