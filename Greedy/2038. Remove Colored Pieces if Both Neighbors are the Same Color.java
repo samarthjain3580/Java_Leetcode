2aa890725698400c65724af4139bf75173bc8918
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-10-08 00:36:56 +0530

    Create 2038. Remove Colored Pieces if Both Neighbors are the Same Color.java

---
+++ Greedy/2038. Remove Colored Pieces if Both Neighbors are the Same Color.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean winnerOfGame(String colors) {
+        int a = 0, b = 0;
+
+        for(int i=1; i<colors.length()-1; i++) {
+            if(colors.charAt(i) == 'A' && colors.charAt(i-1) == 'A' && colors.charAt(i+1) == 'A') {
+                a++;
+            }
+            else if(colors.charAt(i) == 'B' && colors.charAt(i-1) == 'B' && colors.charAt(i+1) == 'B') {
+                b++;
+            }
+        }
+
+        return a>b;
+
+    }
+}
