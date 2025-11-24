b27bf5e7d429275c80cc4cfb567fd135586f550c
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-11-24 19:24:47 +0530

    Create 018. Binary Prefix Divisible By 5.java

---
+++ Bit_Manipulation/018. Binary Prefix Divisible By 5.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> ans = new ArrayList<>();
+        int val = 0;
+
+        for (int i : nums) {
+            val = (val * 2 + i) % 5;
+            ans.add(val == 0);
+        }
+
+        return ans;
+    }
+}
