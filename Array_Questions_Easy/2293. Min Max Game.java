054e8b05d3364be03ad584372368996d730048be
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-12-02 19:05:47 +0530

    Create 2293. Min Max Game.java

---
+++ Array_Questions_Easy/2293. Min Max Game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minMaxGame(int[] nums) {
+        while (nums.length > 1) {
+            int n = nums.length;
+            int[] newNums = new int[n/2];
+            for(int i=0; i<n/2; i++) {
+                if (i % 2 == 0) {
+                    newNums[i] = Math.min(nums[2*i], nums[2*i+1]);
+                }
+                else {
+                    newNums[i] = Math.max(nums[2*i], nums[2*i+1]);
+                }
+            }
+            nums = newNums;
+        }
+        return nums[0];
+    }
+}
