f23a925ddb455b83ead768adb9154d556a734644
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2026-01-02 22:55:36 +0530

    Update 7. Reverse Integer.java

--- Maths_Medium/7. Reverse Integer.java
+++ Maths_Medium/7. Reverse Integer.java
@@ -1,3 +1,21 @@
+//Only Maths Approach
+class Solution {
+    public int reverse(int x) {
+        int len = String.valueOf(x).length();
+        if (x<0) len--;
+        int ans = 0;
+        while(x>0 || x<0) {
+            int rem = x%10;
+            ans += rem * Math.pow(10, len-1);
+            x=x/10;
+            len--;
+        }
+        if (ans == Integer.MAX_VALUE || ans == Integer.MIN_VALUE) return 0;
+        return ans;
+    }
+}
+
+// Using Arraylist
 class Solution {
     public int reverse(int x) {
         List<Integer> list = new ArrayList<>();
