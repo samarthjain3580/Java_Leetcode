2186f3a7e5944e77fb2876a6788d8d2f740c43e3
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2026-01-12 20:48:18 +0530

    Update 1266. Minimum Time Visiting All Points.java

--- Math_Easy/1266. Minimum Time Visiting All Points.java
+++ Math_Easy/1266. Minimum Time Visiting All Points.java
@@ -1,11 +1,12 @@
 class Solution {
     public int minTimeToVisitAllPoints(int[][] points) {
-        int time = 0;
+        int distance = 0;
         for(int i=0; i<points.length-1; i++) {
-            int x = Math.abs(points[i+1][0] - points[i][0]);
-            int y = Math.abs(points[i+1][1] - points[i][1]);
-            time+=Math.max(x,y);
+            int x_diff = Math.abs(points[i][0]-points[i+1][0]);
+            int y_diff = Math.abs(points[i][1]-points[i+1][1]);
+
+            distance += Math.max(x_diff, y_diff);
         }
-        return time;
+        return distance;
     }
 }
