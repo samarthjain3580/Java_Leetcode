b42ca9c2f55c4b5c17fe64e691196f34ef6af248
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2026-01-12 21:47:11 +0530

    Create 1921. Eliminate Maximum Number of Monsters.java

---
+++ Greedy/1921. Eliminate Maximum Number of Monsters.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int eliminateMaximum(int[] dist, int[] speed) {
+        int n = dist.length;
+        double[] time = new double[n];
+        for(int i=0; i<n; i++) {
+            time[i] = (double) dist[i]/speed[i];
+        }
+
+        Arrays.sort(time);
+        int count = 0;
+        for(int i=0; i<n; i++) {
+            if (time[i] > i) count++;
+            else break;
+        }
+        return count;
+    }
+}
