933ff1aac4fa38b2bb087948db418a8498677a83
Samarth jain <153626196+samarthjain3580@users.noreply.github.com>
2026-01-27 14:46:32 +0530

    Create (PQ) 1005. Maximize Sum Of Array After K Negations.java

---
+++ Queue/(PQ) 1005. Maximize Sum Of Array After K Negations.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int num: nums) pq.add(num);
+        while (k-->0) {
+            pq.add(-pq.poll());
+        }
+
+        int sum = 0;
+        for(int num: pq){
+            sum+=num;
+        }
+
+        return sum;
+    }
+}
