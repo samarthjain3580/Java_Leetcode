c9a695d60b5e55c7a129863ad42b18ad55598257
Samarth jain <153626196+samarthjain3580@users.noreply.github.com>
2026-01-27 19:40:07 +0530

    Create 2073. Time Needed to Buy Tickets.java

---
+++ Queue/Easy/2073. Time Needed to Buy Tickets.java
@@ -0,0 +1,22 @@
+// Using Array not queue
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int n = tickets.length;
+        int count = 0;
+
+        if (tickets[k] == 1) return k+1;
+
+        while(tickets[k]>0) {
+            for(int i=0; i<n; i++) {
+                if (tickets[i] != 0) {
+                    tickets[i]--;
+                    count++;
+                }
+
+                if (tickets[k] == 0) return count;
+            }
+        }
+
+        return count;
+    }
+}
