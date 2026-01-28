1f267c03ed3c06f7630f385f41762f835967482c
Samarth jain <153626196+samarthjain3580@users.noreply.github.com>
2026-01-28 18:59:11 +0530

    Create 950. Reveal Cards In Increasing Order.java

---
+++ Queue/Medium/950. Reveal Cards In Increasing Order.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n = deck.length;
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0; i<n; i++) q.add(i);
+
+        Arrays.sort(deck);
+        int[] ans = new int[n];
+
+        for(int i=0; i<n; i++) {
+            ans[q.poll()] = deck[i];
+            q.add(q.poll());
+        }
+
+        return ans;
+    }
+}
