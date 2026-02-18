80213898cdef6575e3445e42b7aace2e1579f5ea
Samarth jain <153626196+samarthjain3580@users.noreply.github.com>
2026-02-18 19:02:31 +0530

    Update 172. Factorial Trailing Zeroes.java

--- Maths_Medium/172. Factorial Trailing Zeroes.java
+++ Maths_Medium/172. Factorial Trailing Zeroes.java
@@ -1,11 +1,10 @@
 class Solution {
     public int trailingZeroes(int n) {
-        int c = 0;
-        while (n>0) {
-            int div = n/5;
-            c+=div;
+        int five = 0;
+        while (n != 0) {
+            five+=(n/5);
             n = n/5;
         }
-        return c;
+        return five;
     }
 }
