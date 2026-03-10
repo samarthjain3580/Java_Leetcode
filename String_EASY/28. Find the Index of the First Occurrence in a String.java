e6ad7cbdf25396b0932ad289932bac8eebd08819
Samarth jain <153626196+samarthjain3580@users.noreply.github.com>
2026-03-10 15:20:52 +0530

    Update 28. Find the Index of the First Occurrence in a String.java

--- String_EASY/28. Find the Index of the First Occurrence in a String.java
+++ String_EASY/28. Find the Index of the First Occurrence in a String.java
@@ -13,3 +13,12 @@ class Solution {
         return ans;
     }
 }
+
+OR
+    
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (!haystack.contains(needle)) return -1;
+        return haystack.indexOf(needle);
+    }
+}
