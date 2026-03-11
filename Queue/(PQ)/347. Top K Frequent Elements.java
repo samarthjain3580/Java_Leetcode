bbc12a9b641c20460e6c52d9335f54d1c9d2d0ab
Samarth jain <153626196+samarthjain3580@users.noreply.github.com>
2026-03-11 12:05:08 +0530

    Create 347. Top K Frequent Elements.java

---
+++ Queue/(PQ)/347. Top K Frequent Elements.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(b)-map.get(a));
+
+        for(int num: map.keySet()) {
+            pq.offer(num);
+        }
+
+        int[] ans = new int[k];
+        for(int i=0; i<k; i++) {
+            ans[i] = pq.poll();
+        }
+
+        return ans;
+    }
+}
