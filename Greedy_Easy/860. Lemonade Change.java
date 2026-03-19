f08646245be5a44034e9d0d6ea07fb923db1b5c5
Samarth jain <153626196+samarthjain3580@users.noreply.github.com>
2026-03-19 10:34:54 +0530

    Update 860. Lemonade Change.java

--- Greedy_Easy/860. Lemonade Change.java
+++ Greedy_Easy/860. Lemonade Change.java
@@ -20,3 +20,29 @@ class Solution {
         return true;
     }
 }
+
+OR 
+
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for(int bill: bills) {
+            if (bill == 5) five++;
+            else if (bill == 10) {
+                ten++;
+                if (five>=1) five--;
+                else return false;
+            }
+            else {
+                if (ten>=1 && five>=1) {
+                    ten--;
+                    five--;
+                }
+                else if (five >=3) five-=3;
+                else return false;
+            }
+        }
+
+        return true;
+    }
+}
